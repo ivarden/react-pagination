47a3deae97b8f4c20228f4e8e7bc67a7b82e733b
Varden Adamchuk <varden.adams@gmail.com>
2020-12-18 23:44:07 -0800

    variant-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(3);
+  const itemLength = items.length;
+
+  const fetchDataApi = async () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  };
+  useEffect(() => {
+    fetchDataApi();
+  }, []);
+
+  const showMoreItems = () => {
+    visible <= itemLength && setVisible(visible + 3);
+  };
+  const scrollUp = () => {
+    window.scrollTo(0, 0);
+  };
+
+  const Cards = () => (
+    <div className="Items-wrapper">
+      {items.slice(0, visible).map(({ userId, id, title, body }) => (
+        <div key={id} data-userid={userId} className="Item-container">
+          <div>
+            <span className="Item-number">{id}</span>
+          </div>
+          <div>
+            <div className="Item-title">{title}</div>
+            <div>{body}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+  const Button = () =>
+    visible <= itemLength ? (
+      <button className="Btn" onClick={showMoreItems}>
+        Load more
+      </button>
+    ) : (
+      <button className="Btn" onClick={scrollUp}>
+        Scroll Up
+      </button>
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="top">
+      <div className="App-container">
+        <header className="App-header"><h2>React pagination variant-01</h2></header>
+      </div>
+      <div className="App-container">
+        <div className="App-wrapper">
+          <Cards />
+          <Button />
+        </div>
+      </div>
     </div>
   );
 }
